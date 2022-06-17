4d537669db9a33b1145c75c09dfdfe094675633d
Zeynep <zeynepyldrm1516@gmail.com>
2022-06-18 01:05:11 +0300

    initial

---
+++ src/main/java/com/example/SpringbootPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPostgresqlApplication.class, args);
+    }
+
+}
